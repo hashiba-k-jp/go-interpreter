fb22255aaece81afcac5dee1459d012dead8240a
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-10-11 01:12:34 +0900

    complete lexer

---
+++ monkey/token/token.go
@@ -0,0 +1,68 @@
+// token/token.go
+
+package token
+
+type TokenType string
+
+type Token struct{
+    Type    TokenType
+    Literal string
+}
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF     = "EOF"
+
+    // identifier + literal
+    IDENT   = "IDENT" // add, foobar, ...
+    INT     = "INT" // 1, 2, 3, ...
+
+    // operand
+    ASSIGN      = "="
+    PLUS        = "+"
+    MINUS       = "-"
+    BANG        = "!"
+    ASTERISK    = "*"
+    SLASH       = "/"
+    LT          = "<"
+    GT          = ">"
+
+    // delimiter
+    COMMA       = "."
+    SEMICOLON   = ";"
+    LPAREN      = "("
+    RPAREN      = ")"
+    LBRACE      = "{"
+    RBRACE      = "}"
+
+    // keywords
+    FUNCTION    = "FUNCTION"
+    LET         = "LET"
+    TRUE        = "TRUE"
+    FALSE       = "FALSE"
+    IF          = "IF"
+    ELSE        = "ELSE"
+    RETURN      = "RETURN"
+
+    EQ          = "=="
+    NOT_EQ      = "!="
+)
+
+// map[<TYPE>]<NAME>
+// Dose it seem to be similar the dict type of python ...?
+var keywords = map[string]TokenType{
+    "fn"    : FUNCTION,
+    "let"   : LET,
+    "true"  : TRUE,
+    "false" : FALSE,
+    "if"    : IF,
+    "else"  : ELSE,
+    "return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType{
+    if tok, ok := keywords[ident]; ok{
+        return tok
+    }
+    return IDENT
+}

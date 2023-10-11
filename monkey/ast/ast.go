434c3cdd3a8d974d93a2c20c315daf89602d294e
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-10-12 01:16:27 +0900

    prefix parse

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -2,10 +2,16 @@
 
 package ast // Abstract Syntax Tree
 
-import "monkey/token"
+import (
+    "monkey/token"
+    "bytes"
+)
 
+
+/* INTERFACE */
 type Node interface{
     TokenLiteral() string
+    String() string
 }
 
 type Statement interface{
@@ -18,10 +24,11 @@ type Expression interface{
     expressionNode()
 }
 
+
+/* PROGRAM */
 type Program struct{
     Statements []Statement
 }
-
 func (p *Program) TokenLiteral() string{
     if len(p.Statements) > 0{
         return p.Statements[0].TokenLiteral()
@@ -29,7 +36,28 @@ func (p *Program) TokenLiteral() string{
         return ""
     }
 }
+func (p *Program) String() string{
+    var out bytes.Buffer
 
+    for _, s := range p.Statements{
+        out.WriteString(s.String())
+    }
+
+    return out.String()
+}
+
+
+/* IDENT */
+type Identifier struct{
+    Token token.Token // token.IDENT
+    Value string
+}
+func (i *Identifier) expressionNode(){}
+func (i *Identifier) TokenLiteral() string{ return i.Token.Literal }
+func (i *Identifier) String() string{ return i.Value }
+
+
+/* LET statement */
 type LetStatement struct{
     Token token.Token // token.LET
     Name *Identifier
@@ -37,10 +65,85 @@ type LetStatement struct{
 }
 func (ls *LetStatement) statementNode(){}
 func (ls *LetStatement) TokenLiteral() string{ return ls.Token.Literal }
+func (ls *LetStatement) String() string{
+    var out bytes.Buffer
 
-type Identifier struct{
-    Token token.Token // token.IDENT
-    Value string
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil{
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
 }
-func (i *Identifier) statementNode(){}
-func (i *Identifier) TokenLiteral() string{ return i.Token.Literal }
+
+
+/* RETURN statement */
+type ReturnStatement struct{
+    Token token.Token
+    ReturnValue Expression
+}
+func (rs *ReturnStatement) statementNode(){}
+func (rs *ReturnStatement) TokenLiteral() string{ return rs.Token.Literal }
+func (rs *ReturnStatement) String() string{
+    var out bytes.Buffer
+
+    out.WriteString(rs.TokenLiteral() + " ")
+
+    if rs.ReturnValue != nil{
+        out.WriteString(rs.ReturnValue.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
+}
+
+
+/* EXPRESSION statement */
+type ExpressionStatement struct{
+    Token token.Token
+    Expression Expression
+}
+func (es *ExpressionStatement) statementNode(){}
+func (es *ExpressionStatement) TokenLiteral() string{ return es.Token.Literal }
+func (es *ExpressionStatement) String() string{
+    if es.Expression != nil{
+        return es.Expression.String()
+    }
+    return ""
+}
+
+
+/* Integer Literal */
+type IntegerLiteral struct{
+    Token token.Token
+    Value int64
+}
+func (il *IntegerLiteral) expressionNode(){}
+func (il *IntegerLiteral) TokenLiteral() string{ return il.Token.Literal }
+func (il *IntegerLiteral) String() string{ return il.Token.Literal }
+
+
+/* Prefix Expression */
+type PrefixExpression struct{
+    Token       token.Token // eg. !, -
+    Operator    string
+    Right       Expression
+}
+func (pe *PrefixExpression) expressionNode(){}
+func (pe *PrefixExpression) TokenLiteral() string{ return pe.Token.Literal }
+func (pe *PrefixExpression) String() string{
+    var out bytes.Buffer
+
+    out.WriteString("(")
+    out.WriteString(pe.Operator)
+    out.WriteString(pe.Right.String())
+    out.WriteString(")")
+
+    return out.String()
+}
--- monkey/parser/parser.go
+++ monkey/parser/parser.go
@@ -7,14 +7,35 @@ import (
     "monkey/lexer"
     "monkey/token"
     "fmt"
+    "strconv"
+)
+
+const (
+    _ int = iota
+    LOWEST
+    EQUALS      // ==
+    LESSGREATER // > or <
+    SUM         // +
+    PRODUCT     // *
+    PREFIX      // -X or !X
+    CALL        // myFunction(X)
+)
+
+type (
+    prefixParseFn func() ast.Expression
+    infixParseFn func(ast.Expression) ast.Expression
+    // the argument is left side of infix
 )
 
 type Parser struct{
-    l *lexer.Lexer
+    l       *lexer.Lexer
+    errors  []string
 
-    curToken  token.Token
-    peekToken token.Token
-    errors []string
+    curToken    token.Token
+    peekToken   token.Token
+
+    prefixParseFns  map[token.TokenType]prefixParseFn
+    infixParseFn    map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser{
@@ -23,11 +44,49 @@ func New(l *lexer.Lexer) *Parser{
         errors: []string{},
     }
 
+    p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+    p.registerPrefix(token.IDENT, p.parseIdentifier)
+    p.registerPrefix(token.INT, p.parseIntegerLiteral)
+    p.registerPrefix(token.BANG, p.parsePrefixExpression)
+    p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+
     // load 2 tokens; curToken and peekToken will be set
     p.nextToken()
     p.nextToken()
 
     return p
+}
+
+func (p *Parser) parseIdentifier() ast.Expression{
+    return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression{
+    lit := &ast.IntegerLiteral{Token: p.curToken}
+
+    value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+    if err != nil{
+        msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+        p.errors = append(p.errors, msg)
+        return nil
+    }
+
+    lit.Value = value
+
+    return lit
+}
+
+func (p *Parser) parsePrefixExpression() ast.Expression{
+    expression := &ast.PrefixExpression{
+        Token:      p.curToken,
+        Operator:   p.curToken.Literal,
+    }
+
+    p.nextToken()
+
+    expression.Right = p.parseExpression(PREFIX)
+
+    return expression
 }
 
 func (p *Parser) nextToken(){
@@ -53,13 +112,15 @@ func (p *Parser) ParseProgram() *ast.Program{
 func (p *Parser) parseStatement() ast.Statement{
     switch p.curToken.Type{
         case token.LET:
-            return p.parserLetStatement()
+            return p.parseLetStatement()
+        case token.RETURN:
+            return p.parseReturnStatement()
         default:
-            return nil
+            return p.parseExpressionStatement()
     }
 }
 
-func (p *Parser)parserLetStatement() *ast.LetStatement{
+func (p *Parser)parseLetStatement() *ast.LetStatement{
     stmt := &ast.LetStatement{Token: p.curToken}
 
     if !p.expectPeek(token.IDENT){
@@ -78,6 +139,43 @@ func (p *Parser)parserLetStatement() *ast.LetStatement{
 
     return stmt
 }
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement{
+    stmt := &ast.ReturnStatement{Token: p.curToken}
+
+    p.nextToken()
+
+    for !p.curTokenIs(token.SEMICOLON){
+        p.nextToken()
+    }
+
+    return stmt
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement{
+    stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+    stmt.Expression = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON){
+        p.nextToken()
+    }
+
+    return stmt
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression{
+    // prefix should be a function
+    prefix := p.prefixParseFns[p.curToken.Type]
+    if prefix == nil{
+        p.noPrefixParseFnError(p.curToken.Type)
+        return nil
+    }
+    leftExp := prefix()
+
+    return leftExp
+}
+
 
 func (p *Parser) curTokenIs(t token.TokenType) bool{
     return p.curToken.Type == t
@@ -103,5 +201,18 @@ func (p *Parser) Errors() []string{
 
 func (p *Parser) peekError(t token.TokenType){
     msg := fmt.Sprintf("expected mext token to be %s, got %s instead", t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn){
+    p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn){
+    p.infixParseFn[tokenType] = fn
+}
+
+func (p *Parser) noPrefixParseFnError(t token.TokenType){
+    msg := fmt.Sprintf("no prefix parse function for %s found", t)
     p.errors = append(p.errors, msg)
 }

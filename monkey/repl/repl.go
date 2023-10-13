adc2f03a8fb5ae9861fb765d67d8c6d6a2b41efa
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-10-13 23:57:39 +0900

    complete parser

--- monkey/repl/repl.go
+++ monkey/repl/repl.go
@@ -7,7 +7,7 @@ import (
     "fmt"
     "io"
     "monkey/lexer"
-    "monkey/token"
+    "monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -16,7 +16,7 @@ func Start(in io.Reader, out io.Writer){
     scanner := bufio.NewScanner(in)
 
     for {
-        fmt.Print(PROMPT)
+        fmt.Printf(PROMPT)
         scanned := scanner.Scan()
         if !scanned{
             return
@@ -24,9 +24,21 @@ func Start(in io.Reader, out io.Writer){
 
         line := scanner.Text()
         l := lexer.New(line)
+        p := parser.New(l)
 
-        for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken(){
-            fmt.Printf("%+v\n", tok)
+        program := p.ParseProgram()
+        if len(p.Errors()) != 0{
+            printParserErrors(out, p.Errors())
+            continue
         }
+
+        io.WriteString(out, program.String())
+        io.WriteString(out, "\n")
+    }
+}
+
+func printParserErrors(out io.Writer, errors []string){
+    for _, msg := range errors{
+        io.WriteString(out, " [ ERROR ] "+msg+"\n")
     }
 }
